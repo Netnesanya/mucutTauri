13203d80fb997df9fd8cdc7613175226e9bf76b4
netnesanya <Netnesanya@mail.ru>
2024-02-17 13:30:24 +0100

    Add "add" temporary url input

--- src/app/songs-list/song/song.component.ts
+++ src/app/songs-list/song/song.component.ts
@@ -3,33 +3,42 @@ import {SongDataFetched} from "../../services/song-data.service";
 import {NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-song',
-  standalone: true,
-  imports: [
-    NgIf
-  ],
-  templateUrl: './song.component.html',
-  styleUrl: './song.component.css'
+    selector: 'app-song',
+    standalone: true,
+    imports: [
+        NgIf
+    ],
+    templateUrl: './song.component.html',
+    styleUrl: './song.component.css'
 })
 export class SongComponent implements OnInit {
-  @Output() removeSong = new EventEmitter<SongDataFetched>();
+    @Output() removeSong = new EventEmitter<SongDataFetched>();
 
 
-  @Input() songData!: SongDataFetched
+    @Input() songData!: SongDataFetched
 
-  constructor() {
-  }
+    public temporaryUrl = ''
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
-  public updateTitle(event: any): void {
-    this.songData['title'] = event.target.value
-  }
+    ngOnInit() {
+    }
 
-  public remove(event: any): void {
-    event.preventDefault()
-    this.removeSong.emit(this.songData)
-    // Remove the song from the parent component
-  }
+    public updateTitle(event: any): void {
+        this.songData['title'] = event.target.value
+    }
+
+    public insertOriginalUrl(event: any): void {
+        this.temporaryUrl = event.target.value
+    }
+
+    public addUlr(): void {
+        this.songData['original_url'] = this.temporaryUrl
+    }
+
+    public remove(event: any): void {
+        event.preventDefault()
+        this.removeSong.emit(this.songData)
+    }
 }
